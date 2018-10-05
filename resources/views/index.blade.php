0e55da21e5cc30b5659bbb501710bc900a94e3ed
Sebastian <desarrollo@lagobo.com>
2018-10-05 14:24:15 -0500

    Continuación de maquetación

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,4 +10,25 @@
 			</div>
 		@endforeach
 	</div>
+
+	<div id="conoce">
+		<h2 class="conoce-title">Conoce todos los servicios <br> que tenemos para ti</h2>
+		<div class="row">
+			<div class="col-md-12 col-lg-2 offset-lg-1 text-center">
+				<img src="images/conoce-oportuyaImagen.png" alt="" class="img-fluid" />
+			</div>
+			<div class="col-md-12 col-lg-2 text-center">
+				<img src="images/conoce-motoImagen.png" alt="" class="img-fluid" />
+			</div>
+			<div class="col-md-12 col-lg-2 text-center">
+				<img src="images/conoce-libranzaImagen.png" alt="" class="img-fluid" />
+			</div>
+			<div class="col-md-12 col-lg-2 text-center">
+				<img src="images/conoce-segurosImagen.png" alt="" class="img-fluid" />
+			</div>
+			<div class="col-md-12 col-lg-2 text-center">
+				<img src="images/conoce-viajesImagen.png" alt="" class="img-fluid" />
+			</div>
+		</div>
+	</div>
 @endsection
