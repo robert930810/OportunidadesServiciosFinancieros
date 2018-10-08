d2d59f20de50d5982615bd79b1ff5bfe85dcac53
Robert Garcia <robert930810@gmail.com>
2018-10-08 17:58:28 -0500

    Inicio de administrador de archivos

--- app/Page.php
+++ app/Page.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
-    //
+    protected $table = 'pages';
+ 
+    //protected $timestamp = false; remove if you want no timestamp on table
+ 
+    protected $fillable = ['name', 'description','content'];
 }
---
+++ resources/views/pages/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+ 
+@section('content')
+    <h2> Create new Page </h2>
+    <hr>
+ 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form method="post" action="{{ route('pages.store') }}">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label>Page name</label>
+            <input type="text" name="title" class="form-control" placeholder="Page name">
+        </div>
+        <div class="form-group">
+            <label>Page Description</label>
+            <textarea name="body" rows="4" class="form-control"></textarea>
+        </div>
+        <a href="{{ route('pages.index') }}" class="btn btn-default">Cancel</a>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+ 
+@stop
