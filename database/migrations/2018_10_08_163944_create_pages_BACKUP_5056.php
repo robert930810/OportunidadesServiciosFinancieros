8958f4664043a941b6736b18b13df80cd65cc21e
Robert Garcia <robert930810@gmail.com>
2018-10-09 14:52:18 -0500

    Commit arreglo

---
+++ database/migrations/2018_10_08_163944_create_pages_BACKUP_5056.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+<<<<<<< HEAD
+            $table->integer('idUser')->unsigned()->nullable();
+=======
+            $table->integer('idUser')->nullable()->unsigned();
+>>>>>>> dev1
+            $table->integer('idCategory')->nullable();
+            $table->string('description')->nullable();
+            $table->string('content')->nullable();
+            $table->string('url')->unique()->nullable();
+        });
+
+        Schema::table('pages',function(Blueprint $table){
+                $table->foreign('idUser')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
